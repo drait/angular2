aec711b5f016776dfe18b52c9f4fd447dfdefcef
Dris Ait Oujkal <xoujkdr@ville.montreal.qc.ca>
2016-11-04 16:34:16 -0400

    add http support - search bug fix

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,25 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
-import { Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
     selector: 'my-dashboard',
-    template: 'dashboard.component.html',
+    templateUrl: 'dashboard.component.html',
     styleUrls: ['dashboard.component.css']
 })
-
 export class DashboardComponent implements OnInit {
     heroes: Hero[] = [];
 
-    constructor(private router: Router, private HeroService: HeroService) {
-
+    constructor(
+        private router: Router,
+        private heroService: HeroService) {
     }
 
     ngOnInit(): void {
-        this.HeroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
     gotoDetail(hero: Hero): void {
